98029b7b0ddc17aed545d178d54a943b66cac91a
adityakumarsahu108 <adityakumarsahu108@gmail.com>
2024-06-20 16:05:22 +0530

    created community and added drawer in the home screen ui

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:flutter_reddit_clone/features/home/drawers/community_list_drawer.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
+
+  void displayDrawer(BuildContext context) {
+    Scaffold.of(context).openDrawer();
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -12,10 +17,12 @@ class HomeScreen extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Home'),
         centerTitle: false,
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {},
-        ),
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => displayDrawer(context),
+          );
+        }),
         actions: [
           IconButton(
             onPressed: () {},
@@ -29,6 +36,7 @@ class HomeScreen extends ConsumerWidget {
           ),
         ],
       ),
+      drawer: const CommunityListDrawer(),
     );
   }
 }
--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:flutter_reddit_clone/features/community/screens/create_community_screen.dart';
 import 'package:flutter_reddit_clone/features/home/screens/home_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
@@ -9,4 +10,5 @@ final loggedOutRoute = RouteMap(routes: {
 
 final loggedInRoute = RouteMap(routes: {
   '/': (_) => const MaterialPage(child: HomeScreen()),
+   '/create-community': (_) => const MaterialPage(child: CreateCommunityScreen()),
 });
