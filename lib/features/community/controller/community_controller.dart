13b1892b5166ac1b83297d883c352959d96e5bf3
adityakumarsahu108 <adityakumarsahu108@gmail.com>
2024-06-22 18:00:26 +0530

    displayed all the user created communites in the drawer

--- lib/features/community/controller/community_controller.dart
+++ lib/features/community/controller/community_controller.dart
@@ -7,6 +7,11 @@ import 'package:flutter_reddit_clone/models/community_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:routemaster/routemaster.dart';
 
+final userCommunitiesProvider = StreamProvider((ref) {
+  final communityController = ref.watch(communityControllerProvider.notifier);
+  return communityController.getUserCommunities();
+});
+
 final communityControllerProvider =
     StateNotifierProvider<CommunityController, bool>((ref) {
   final communityRepository = ref.watch(communityRepositoryProvider);
@@ -41,5 +46,10 @@ class CommunityController extends StateNotifier<bool> {
       showSnackBar(context, 'Community created successfully');
       Routemaster.of(context).pop();
     });
+  }
+
+  Stream<List<Community>> getUserCommunities() {
+    final uid = _ref.read(userProvider)!.uid;
+    return _communityRepository.getUserCommunities(uid);
   }
 }
--- lib/features/home/drawers/community_list_drawer.dart
+++ lib/features/home/drawers/community_list_drawer.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reddit_clone/core/common/error_text.dart';
+import 'package:flutter_reddit_clone/core/common/loader.dart';
+import 'package:flutter_reddit_clone/features/community/controller/community_controller.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:routemaster/routemaster.dart';
 
@@ -20,6 +23,26 @@ class CommunityListDrawer extends ConsumerWidget {
               leading: const Icon(Icons.add),
               onTap: () => navigateToCreateCommunity(context),
             ),
+            ref.watch(userCommunitiesProvider).when(
+                  data: (communities) => Expanded(
+                    child: ListView.builder(
+                      itemCount: communities.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final community = communities[index];
+                        return ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(community.avatar),
+                          ),
+                          title: Text('r/${community.name}'),
+                          onTap: () {},
+                        );
+                      },
+                    ),
+                  ),
+                  error: (error, stackTrace) =>
+                      ErrorText(error: error.toString()),
+                  loading: () => const Loader(),
+                ),
           ],
         ),
       ),
