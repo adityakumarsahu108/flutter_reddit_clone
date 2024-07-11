694cf0dfbec1f9858cbaa227c2f7743f926e369c
adityakumarsahu108 <adityakumarsahu108@gmail.com>
2024-07-11 10:01:26 +0530

    warnings removed

--- lib/features/post/screens/add_post_screen.dart
+++ lib/features/post/screens/add_post_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:flutter_reddit_clone/theme/pallete.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
